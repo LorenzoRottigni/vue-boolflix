54bb0793ea0fd9ee2bde5d4971ff782c8fe53dfd
LorenzoRottigni <lorenzo@rottigni.net>
2021-11-17 17:51:47 +0100

    img src load

--- src/components/DataLoader.vue
+++ src/components/DataLoader.vue
@@ -3,7 +3,8 @@
         <div :key="data.id" v-for="data in queryData">
             <h1>name:{{data.name}}</h1>
             <h1>id:{{data.id}}</h1>
-            <h1>poster_path:{{data.poster_path}}</h1>
+            
+            <img :src="'https://image.tmdb.org/t/p/w500' + data.poster_path" alt="">
             <h1>first_air_date:{{data.first_air_date}}</h1>
             <h1>origin_country:{{data.origin_country}}</h1>
         </div>
